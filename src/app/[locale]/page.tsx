b5cc3e8895381591c42c59647cabe9cd3000b6a9
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-08-26 13:35:05 +0700

    fix: make skip to main content works

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,10 @@
-import { ModeToggle } from "@/components/darkmode-switcher";
-import { Button } from "@/components/ui/button";
 import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
 	const t = await getTranslations("Home");
 	return (
-		<main className="flex gap-4 items-center justify-center w-full h-full">
-			<Button>{t("This is a button")}</Button>
-			<ModeToggle />
+		<main id="main" className="flex gap-4 items-center justify-center w-full">
+			<h1>deliver Secure and Accessible web app</h1>
 		</main>
 	);
 }
--- src/components/nav/skip-to-main-content.tsx
+++ src/components/nav/skip-to-main-content.tsx
@@ -1,6 +1,5 @@
 import { getTranslations } from "next-intl/server";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export async function SkipToMainContent() {
 	const t = await getTranslations("Layout");
@@ -9,12 +8,12 @@ export async function SkipToMainContent() {
 			asChild
 			className="absolute left-2 top-2 h-0 overflow-hidden px-0 py-0 z-50"
 		>
-			<Link
+			<a
 				href="#main"
 				className="underline overflow-hidden focus-within:h-fit focus-within:p-4 p-0 focus:outline-none "
 			>
 				{t("Skip to main content")}
-			</Link>
+			</a>
 		</Button>
 	);
 }
