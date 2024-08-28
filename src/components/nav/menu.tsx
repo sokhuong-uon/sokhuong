93e20b8a749e29dd6cebf8b2b551be96ebea5133
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-08-28 16:33:08 +0700

    fix: make buttons larger

--- src/components/nav/menu.tsx
+++ src/components/nav/menu.tsx
@@ -7,7 +7,7 @@ export function Menu() {
 	return (
 		<nav className="w-full container border-b py-4 bg-white dark:bg-black backdrop-blur-sm flex">
 			<div>
-				<Button asChild variant={"link"}>
+				<Button asChild variant={"link"} size={"lg"}>
 					<Link href="/">
 						<DnaIcon className="scale-125" />
 						<p className="sr-only">Home</p>
@@ -16,12 +16,22 @@ export function Menu() {
 			</div>
 			<ul className="ml-auto flex gap-4">
 				<li>
-					<Button asChild variant={"link"} className="focus-within:underline">
+					<Button
+						asChild
+						variant={"link"}
+						size={"lg"}
+						className="focus-within:underline"
+					>
 						<Link href="/about">About</Link>
 					</Button>
 				</li>
 				<li>
-					<Button asChild variant={"link"} className="focus-within:underline">
+					<Button
+						asChild
+						variant={"link"}
+						size={"lg"}
+						className="focus-within:underline"
+					>
 						<Link href="/sketch">Sketch</Link>
 					</Button>
 				</li>
