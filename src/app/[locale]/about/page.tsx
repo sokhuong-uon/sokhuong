6107474d4f17e53bb207affdabd400474dd72fc4
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-08-28 15:54:33 +0700

    feat: init about page

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,7 @@
+export default async function About() {
+	return (
+		<main id="main" className="flex items-center justify-center w-full">
+			<div className="py-4 flex gap-4">About Page</div>
+		</main>
+	);
+}
