e91a426aa34dfa6d1d2dbb9c2f72c50a4e8c40be
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-08-29 21:38:51 +0700

    refactor: create hero section component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { HeroSection } from "@/components/hero/hero-section";
 import { getTranslations } from "next-intl/server";
-import { Archivo_Black } from "next/font/google";
-import Link from "next/link";
-const archivoBlack = Archivo_Black({ subsets: ["latin"], weight: ["400"] });
 
 export default async function Home() {
 	const t = await getTranslations("Home");
@@ -11,31 +8,7 @@ export default async function Home() {
 			id="main"
 			className="flex items-center justify-center w-full flex-col"
 		>
-			<div className="max-w-3xl py-24 h-fit  max-h-[100vw] flex flex-col items-center justify-center gap-10">
-				<h1
-					className={`${archivoBlack.className} sm:text-6xl text-balance font-bold antialiased text-center text-3xl xs:text-4xl text-neutral-400 contrast-more:text-neutral-300 [&>em]:text-foreground`}
-				>
-					Build software applications we could{" "}
-					<span className="text-primary">rely on</span>
-				</h1>
-				<p className="text-lg sm:text-xl lg:text-2xl max-w-2xl text-neutral-400 contrast-more:text-neutral-300 text-center text-balance">
-					Hi! I am Sokhuong. A software engineer, technical writer, and the one
-					who pay attention to details.
-				</p>
-				<div className="flex gap-4">
-					<Button size={"lg"} asChild className="focus:underline">
-						<Link href="/sketch">See my sketches</Link>
-					</Button>
-					<Button
-						size={"lg"}
-						variant={"outline"}
-						asChild
-						className="focus:underline"
-					>
-						<Link href="/about#contact">Let&apos;s talk</Link>
-					</Button>
-				</div>
-			</div>
+			<HeroSection />
 		</main>
 	);
 }
