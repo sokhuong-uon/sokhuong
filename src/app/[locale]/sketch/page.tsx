5f8c9d83fc9ab572e539a33dfbd0c4f509719f9e
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-09-01 19:28:52 +0700

    feat: add sketch and sketch detail page

--- src/app/[locale]/sketch/page.tsx
+++ src/app/[locale]/sketch/page.tsx
@@ -1,15 +1,30 @@
 import { ModeToggle } from "@/components/darkmode-switcher";
+import { SketchCard } from "@/components/sketch/sketch-card";
 import { Button } from "@/components/ui/button";
 import { getTranslations } from "next-intl/server";
 
 export default async function Sketch() {
 	const t = await getTranslations("Home");
+	const sketches = [
+		"A text editor built using Tiptap.",
+		"A simple 3D editor using ThreeJS and React-three-fiber.",
+		"A simple chat app using SocketIO.",
+	];
 	return (
 		<main id="main" className="flex items-center justify-center w-full">
-			<div className="py-4 flex gap-4">
-				<Button>{t("This is a button")}</Button>
-				<ModeToggle />
-			</div>
+			<ul
+				className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 border w-full container py-16"
+				aria-label="Sketches -- A list of things that I built for learning purpose."
+			>
+				{sketches.map((title) => (
+					<li key={title} aria-labelledby="sketch-title">
+						<SketchCard
+							title={title}
+							description="A simple counter app."
+						></SketchCard>
+					</li>
+				))}
+			</ul>
 		</main>
 	);
 }
