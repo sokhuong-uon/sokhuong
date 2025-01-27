e8270e92ce37559442d25f5337b9a7a7bd38a620
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-01-27 18:57:31 +0700

    fix: correct sketches link

--- src/app/[locale]/sketch/page.tsx
+++ src/app/[locale]/sketch/page.tsx
@@ -10,6 +10,7 @@ export default async function Sketch() {
 		{
 			title: "Multi-page form",
 			description: "A form wizard using React-hook-form and framer-motion.",
+			href: "/sketch/multi-page-form-wizard/personal-information",
 		},
 	];
 
@@ -19,9 +20,9 @@ export default async function Sketch() {
 				className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 w-full container py-16"
 				aria-label="Sketches -- A list of things that I built for learning purpose."
 			>
-				{sketches.map(({ title, description }) => (
+				{sketches.map(({ title, description, href }) => (
 					<li key={title} aria-labelledby="sketch-title">
-						<Link href="/sketch/multi-page-form-wizard/personal-information">
+						<Link href={href}>
 							<SketchCard title={title} description={description}></SketchCard>
 						</Link>
 					</li>
