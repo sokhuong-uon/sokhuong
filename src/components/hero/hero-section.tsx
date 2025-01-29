30259d0881a0089b00aa912ed7abc58585197b44
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-01-29 14:02:44 +0700

    fix(a11y): improve contrast in hero section

--- src/components/hero/hero-section.tsx
+++ src/components/hero/hero-section.tsx
@@ -6,13 +6,14 @@ const archivoBlack = Archivo_Black({ subsets: ["latin"], weight: ["400"] });
 
 export function HeroSection() {
 	return (
-		<div className="max-w-3xl py-24 lg:py-36 h-fit  max-h-[100vw] flex flex-col items-center justify-center gap-10">
+		<div className="py-24 lg:py-36 h-fit max-h-[100vw] flex flex-col items-center justify-center gap-10 container">
 			<h1
-				className={`${archivoBlack.className} sm:text-6xl text-balance font-bold antialiased text-center text-3xl xs:text-4xl text-neutral-400 contrast-more:text-neutral-300 [&>em]:text-foreground`}
+				className={`${archivoBlack.className} sm:text-6xl text-balance font-bold antialiased text-center text-3xl xs:text-4xl  [&>em]:text-foreground`}
 			>
-				Build <span className="text-primary">reliable</span> web applications
+				Build <span className="text-blue-600 dark:text-blue-500">reliable</span>{" "}
+				software
 			</h1>
-			<p className="text-lg sm:text-xl lg:text-2xl max-w-2xl text-neutral-400 contrast-more:text-neutral-300 text-center text-balance">
+			<p className="sm:text-xl lg:text-2xl max-w-96 sm:max-w-2xl text-center text-balance">
 				Hi! I&apos;m Sokhuong; a software engineer and a technical writer
 				wannabe 😄
 			</p>
